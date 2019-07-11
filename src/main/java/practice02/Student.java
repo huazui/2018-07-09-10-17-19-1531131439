7460b43b98f6f5c51b1db6090997f444b4abff6b
huazui <2509155771@qq.com>
2019-07-12 00:41:12 +0800

    Add files via upload

--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,40 @@
 package practice02;
 
-public class Student {
+public class Student entends Person {
+    private String name;
+    private int age;
+    int kClass;
+    public Student(String name,int age,int kLass){
+        this.name=name;
+        this.age=age;
+        this.kClass= kLass;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+    public void setkClass(int kClass) {
+        this.klass = kClass;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+    public String introduce(){
+        return "I am a Student. I am at Class "+this.getKlass()+".");
+    }
 }
