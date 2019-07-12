1cf231cd81575a46e998709129a41c49e08c547f
huazui <2509155771@qq.com>
2019-07-12 18:03:02 +0800

    Add files via upload

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,53 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+    private String name;
+    private int age;
+    int klass=0;
+    public Teacher(){}
+    public Teacher(String name,int age){
+        this.name=name;
+        this.age=age;
+
+    }
+    public Teacher(String name,int age,int klass){
+        this.name=name;
+        this.age=age;
+        this.klass=klass;
+
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+    public void setKlass(int kClass) {
+        this.klass = kClass;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+    public String introduce(){
+        String a=super.introduce()+" I am a Teacher.";
+        String b;
+        if(this.getKlass()==0)
+            b=(" I teach No Class.");
+        else b=" I teach Class "+getKlass()+".";
+        return a+b;
+    }
 }
+
