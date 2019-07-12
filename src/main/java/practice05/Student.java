f51d133404ffc1571334542692f9ac2231101d84
huazui <2509155771@qq.com>
2019-07-12 18:02:22 +0800

    Add files via upload

--- src/main/java/practice05/Student.java
+++ src/main/java/practice05/Student.java
@@ -1,4 +1,43 @@
 package practice05;
 
-public class Student {
+public class Student extends Person {
+    private String name;
+    private int age;
+    int klass;
+    public Student(String name,int age,int kClass){
+        this.name=name;
+        this.age=age;
+        this.klass=kClass;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+    public void setKlass(int kClass) {
+        this.klass = kClass;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+    public String introduce(){
+        String a=super.introduce();
+        String b=(" I am a Student. I am at Class "+this.getKlass()+".");
+        return a+b;
+    }
 }
--- src/main/java/practice05/Worker.java
+++ src/main/java/practice05/Worker.java
@@ -1,4 +1,32 @@
 package practice05;
 
-public class Worker {
+public class Worker extends Person {
+    public Worker(){}
+    public Worker(String name,int age){
+        this.name=name;
+        this.age=age;
+    }
+    private String name;
+    private int age;
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String introduce(){
+        String a=super.introduce();
+        String b=" I am a Worker. I have a job.";
+        return a+b;
+    }
 }
