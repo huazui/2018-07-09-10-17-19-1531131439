19f538f86e49faca555bad8f9aeba910d7623022
huazui <2509155771@qq.com>
2019-07-12 18:01:54 +0800

    Add files via upload

--- src/main/java/practice04/Worker.java
+++ src/main/java/practice04/Worker.java
@@ -1,4 +1,32 @@
 package practice04;
 
-public class Worker {
+public class Worker extends Person{
+    public Worker(){}
+    public Worker(String name,int age){
+        this.name=name;
+        this.age=age;
+    }
+    private String name;
+    private int age;
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String introduce(){
+        String a=super.baseIntroduce();
+        String b=" I am a Worker. I have a job.";
+        return a+b;
+    }
 }
