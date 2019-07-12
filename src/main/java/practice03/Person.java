ed5f01f9d390b784595705d37d598757b4c7f92c
huazui <2509155771@qq.com>
2019-07-12 18:01:12 +0800

    Add files via upload

--- src/main/java/practice03/Person.java
+++ src/main/java/practice03/Person.java
@@ -1,4 +1,30 @@
 package practice03;
 
 public class Person {
+    public Person(){}
+    public Person(String name,int age){
+        this.name=name;
+        this.age=age;
+    }
+    private String name;
+    private int age;
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String introduce(){
+        return ("My name is "+this.getName()+". I am "+this.getAge()+" years old.");
+    }
 }
--- src/main/java/practice03/Worker.java
+++ src/main/java/practice03/Worker.java
@@ -1,4 +1,31 @@
 package practice03;
 
 public class Worker {
+    public Worker(){}
+    public Worker(String name,int age){
+        this.name=name;
+        this.age=age;
+    }
+    private String name;
+    private int age;
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String introduce(){
+        return ("I am a Worker. I have a job.");
+    }
 }
+
