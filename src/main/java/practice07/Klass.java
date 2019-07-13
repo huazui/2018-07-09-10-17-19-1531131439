6cc8d792164d15623e43347747f829fc8e2dbe01
huazui <2509155771@qq.com>
2019-07-13 23:50:02 +0800

    Add files via upload
    I have finish one-six quickly,seven takes 15 min.

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,19 @@
 package practice07;
 
 public class Klass {
+    private int number;
+    public Klass(){}
+    public Klass(int number){
+        this.number=number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+    public String getDisplayName(){
+        return "Class "+this.getNumber();
+    }
+    public int getNumber() {
+        return number;
+    }
 }
--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -1,4 +1,49 @@
 package practice07;
 
-public class Student {
+public class Student extends Person {
+    private String name;
+    private int age;
+
+    Klass klass;
+    public Student(){}
+    public Student(String name,int age,Klass kClass){
+
+        this.name=name;
+        this.age=age;
+        this.klass=kClass;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+
+
+    @Override
+    public String getName() {
+        return name;
+    }
+    public Klass getKlass() {
+        return klass;
+    }
+
+
+    public int getAge() {
+        return age;
+    }
+
+
+    public String introduce(){
+        String a=super.introduce();
+        String b=(" I am a Student. I am at Class "+this.getKlass().getNumber()+".");
+        return a+b;
+    }
 }
