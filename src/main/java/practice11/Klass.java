025f66d318e2061df77043b2b8103763d2ec9f82
huazui <2509155771@qq.com>
2019-07-13 23:58:01 +0800

    Add files via upload
    Excluding the test time, the function of adding notifications took me less than ten minutes. Although I have not studied the observer mode, I can do it quickly with my own method. Finally I learned about the observer pattern and found that it is very similar to my thoughts. Just the observer pattern has one more observer class to decouple two classes.

--- src/main/java/practice11/Klass.java
+++ src/main/java/practice11/Klass.java
@@ -1,4 +1,50 @@
 package practice11;
 
 public class Klass {
+    private Student leader;
+    private int number;
+    private Teacher teacher;
+    public Klass(){}
+    public Klass(int number){
+        this.number=number;
+    }
+    public void assignLeader(Student leader){
+        if(leader.getKlass().getNumber()==this.getNumber()) {
+            this.leader = leader;
+            System.out.print("I am "+this.getTeacher().getName()+". I know "+leader.getName()+" become Leader of Class "+this.getNumber()+".\n");
+        }
+        else {
+            System.out.print("It is not one of us.");
+            System.out.print("\n");
+        }
+    }
+
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+
+    public void appendMember(Student student){
+        student.setKlass(this);
+        System.out.print("I am "+this.getTeacher().getName()+". I know "+student.getName()+" has joined Class "+this.getNumber()+".\n");
+
+
+    }
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+    public String getDisplayName(){
+        return "Class "+this.getNumber();
+    }
+    public int getNumber() {
+        return number;
+    }
 }
